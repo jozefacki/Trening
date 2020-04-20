ff9146de91b35531b49c2f3aba2df7dfe3ac1dea
Katarzyna Hoppe <A9WKXZZ@mmm.com>
2020-04-20 12:53:54 +0200

    Typy generyczne

---
+++ 3mTrening/Generyki.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3mTrening
+{
+    class Generyki
+    {
+        public int Suma(int a, int b)
+        {
+            return a + b;
+        }
+
+        public double SumaPrzecinek (double c, double d)
+        {
+            return c + d;
+        }
+
+        public decimal SumaPrzecinek1 (decimal e, decimal f)
+        {
+            return e + f;
+        }
+
+        public <T> SumaGeneryczna (<T> e, <T> f)
+        {
+            return e + f;
+        }
+
+        List<T> ListaGeneryczna = new List<T>();
+
+    }
+}
