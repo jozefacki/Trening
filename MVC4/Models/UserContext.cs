fe036a3b053c86f56a52f6a0dc8152e9a4b23120
Piotr Jozefacki <A96N9ZZ@mmm.com>
2020-04-20 14:56:11 +0200

    UserContext add using

--- MVC4/Models/UserContext.cs
+++ MVC4/Models/UserContext.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity.Infrastructure;
 
 namespace MVC4.Models
 {
     public class UserContext : DbContext
     {
-        public DbSet <UserViewModel> Users { get; set; }
+        public DbSet<UserViewModel> Users { get; set; }
 
     }
 }
